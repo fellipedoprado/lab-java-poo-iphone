27592968ddd6af234b24dcc9bff17f367839f933
fellipedoprado <fellipe.doprado@gmail.com>
2025-04-19 01:35:55 -0300

    adds solution code to proposed uml scheme

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import com.dio.iphone.classes.iPhone;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        iPhone meuIphone = new iPhone();
+
+        meuIphone.selecionarMusica("We Will Rock You - Queen");
+        meuIphone.tocar();
+        meuIphone.pausar();
+
+        meuIphone.ligar("11999999999");
+        meuIphone.atender();
+        meuIphone.iniciarCorreioVoz();
+
+        meuIphone.exibirPagina("https://www.apple.com");
+        meuIphone.adicionarNovaAba();
+        meuIphone.atualizarPagina();
     }
 }
+
